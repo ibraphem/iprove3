b1617fd198c929b44301224548494458002262ef
Yaqub <yaqub.adesola@gmail.com>
2021-11-25 14:03:04 +0000

    Adding api_docs_first_part

--- src/markup/pages/api_documentation/layouts/ApiSide.jsx
+++ src/markup/pages/api_documentation/layouts/ApiSide.jsx
@@ -1,8 +1,9 @@
 import './apiSidebar.css';
 import { Link } from 'react-router-dom';
+import Sticky from 'react-stickynode';
 function ApiSide(props) {
     return (
-        <>
+        <Sticky>
             <aside className="sticky-top pb-1 sideBar">
                 <div className="widget">
 
@@ -11,19 +12,19 @@ function ApiSide(props) {
                         <br />
                         <h6>GENERAL</h6>
                         <li ><Link to="/api/getting-started"><span>Getting Started</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Introduction</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Authentication</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>About Verifications</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/introduction"><span>Introduction</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/authentication"><span>Authentication</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/about-verification"><span>About Verifications</span><i className="fa fa-angle-right"></i></Link></li>
                         <br />
                         <h6>IDENTITY VERIFICATIONS</h6>
-                        <li><Link to="/service-detail"><span>Bank Verification Numbers (BVN)</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Corporate Affairs Commission</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Driver License</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Identity Biometrics</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Identity Matching</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>National Identity</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Tax Identification Number</span><i className="fa fa-angle-right"></i></Link></li>
-                        <li><Link to="/service-detail"><span>Voters Card</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/bvn"><span>Bank Verification Numbers (BVN)</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/cac"><span>Corporate Affairs Commission</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/driver-licence"><span>Driver License</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/identity-biometrics"><span>Identity Biometrics</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/identity-matching"><span>Identity Matching</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/nin"><span>National Identity</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/tin"><span>Tax Identification Number</span><i className="fa fa-angle-right"></i></Link></li>
+                        <li><Link to="/api/voterscard"><span>Voters Card</span><i className="fa fa-angle-right"></i></Link></li>
                         <br />
                         <h6>ADDRESS VERIFICATIONS</h6>
                         <li><Link to="/service-detail"><span>Submit Address Verification</span><i className="fa fa-angle-right"></i></Link></li>
@@ -34,7 +35,7 @@ function ApiSide(props) {
                     </ul>
                 </div>
             </aside>
-        </>
+        </Sticky>
     );
 }
 
